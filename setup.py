b683ed5288b39f785272d93d35da4ed6ba1a262b
Daewon Lee <daewon4you@gmail.com>
2023-01-05 18:15:59 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='fasttenet',
+      version='0.0.1',
+      description='FastTENET',
+      url='http://github.com/cxinsys/fasttenet',
+      author='Complex Intelligent Systems Laboratory (CISLAB)',
+      author_email='daewon4you@gmail.com',
+      license='BSD-3-Clause',
+      packages=find_packages(),
+      install_requires=['numpy'],
+      zip_safe=False,)
