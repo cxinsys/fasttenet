362a75126c97cc3443b879c976d7296b1c8ac16f
Rakbin Sung <sac3561488@gamil.com>
2023-02-17 14:56:59 +0900

    Revise tutorial

--- tutorials/tutorial01.py
+++ tutorials/tutorial01.py
@@ -19,14 +19,19 @@ if __name__ == "__main__":
     # expression data, trajectory data, branch data path is required
     # tf data path is optional
     # save path is optional
-    worker = fte.FastTENET(dpath_exp_data=dpath_exp_data,
-                           dpath_trj_data=dpath_trj_data,
-                           dpath_branch_data=dpath_branch_data,
-                           dpath_tf_data=dpath_tf_data,
-                           spath_result_matrix=spath_result_matrix)
+    worker = fte.FastTENET(dpath_exp_data=dpath_exp_data, # Required
+                           dpath_trj_data=dpath_trj_data, # Required
+                           dpath_branch_data=dpath_branch_data, # Required
+                           dpath_tf_data=dpath_tf_data, # Required
+                           spath_result_matrix=spath_result_matrix) # Optional
 
-    # if device_ids is not refined, use whole gpus
-    result_matrix = worker.work(device='gpu', device_ids=[0, 1, 2, 3, 4, 5, 6, 7],
-                                batch_size=2 ** 15)
+    result_matrix = worker.work(device='gpu', # Optional, default: 'cpu'
+                                device_ids=[0, 1, 2, 3, 4, 5, 6, 7], # Optional, if device is 'gpu', use whole gpus
+                                batch_size=2 ** 15, # Required
+                                kp=0.5, # Optional, default: 0.5
+                                percentile=0, # Optional, default: 0
+                                win_length=10, # Optional, default: 10
+                                polyorder=3 # Optional, default: 3
+                                )
 
     print(result_matrix)
