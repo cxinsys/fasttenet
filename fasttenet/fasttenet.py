b964305d73dc02512c20bedda242f75dadb55136
rbsung <rakbin007@naver.com>
2024-03-28 00:19:42 +0900

    Add the ability to set parameters via YAML

--- fasttenet/fasttenet.py
+++ fasttenet/fasttenet.py
@@ -1,3 +1,5 @@
+import os
+import os.path as osp
 import time
 import math
 from itertools import permutations
@@ -13,42 +15,53 @@ from fasttenet.utils import get_device_list
 
 class FastTENET(object):
     def __init__(self,
-                 device=None,
-                 device_ids=None,
-                 procs_per_device=None,
                  dpath_exp_data=None,
                  dpath_trj_data=None,
                  dpath_branch_data=None,
                  dpath_tf_data=None,
                  spath_result_matrix=None,
-                 batch_size=None,
-                 kp=0.5,
-                 percentile=0,
-                 win_length=10,
-                 polyorder=3,
-                 make_binary=False
+                 make_binary=False,
+                 config=None,
                  ):
 
-        if not dpath_exp_data:
-            raise ValueError("Expression data should be refined")
-        if not dpath_trj_data:
-            raise ValueError("Trajectory should be refined")
-        if not dpath_branch_data:
-            raise ValueError("Branch data should be refined")
-
-        self._node_name, self._exp_data = load_exp_data(dpath_exp_data, make_binary)
-        self._trajectory = load_time_data(dpath_trj_data, dtype=np.float32)
-        self._branch = load_time_data(dpath_branch_data, dtype=np.int32)
-
         self._tf = None
-
-        if dpath_tf_data is not None:
-            self._tf = np.loadtxt(dpath_tf_data, dtype=str)
-
-        self._spath_result_matrix = spath_result_matrix
-
         self._refined_exp_data = None
         self._result_matrix = None
+
+        if config:
+            inits = config['INIT']
+            droot = inits['DROOT']
+            dpath_exp_data = osp.join(droot, inits['FPATH_EXP'])
+            dpath_trj_data = osp.join(droot, inits['FPATH_TRJ'])
+            dpath_branch_data = osp.join(droot, inits['FPATH_BRANCH'])
+            dpath_tf_data = osp.join(droot, inits['FPATH_TF'])
+            make_binary = bool(inits['MAKE_BINARY'])
+
+            self._node_name, self._exp_data = load_exp_data(dpath_exp_data, make_binary)
+            self._trajectory = load_time_data(dpath_trj_data, dtype=np.float32)
+            self._branch = load_time_data(dpath_branch_data, dtype=np.int32)
+
+            if dpath_tf_data is not None:
+                self._tf = np.loadtxt(dpath_tf_data, dtype=str)
+
+            self._spath_result_matrix = osp.join(droot, inits['SPATH_RESULT'])
+
+        else:
+            if not dpath_exp_data:
+                raise ValueError("Expression data should be refined")
+            if not dpath_trj_data:
+                raise ValueError("Trajectory should be refined")
+            if not dpath_branch_data:
+                raise ValueError("Branch data should be refined")
+
+            self._node_name, self._exp_data = load_exp_data(dpath_exp_data, make_binary)
+            self._trajectory = load_time_data(dpath_trj_data, dtype=np.float32)
+            self._branch = load_time_data(dpath_branch_data, dtype=np.int32)
+
+            if dpath_tf_data is not None:
+                self._tf = np.loadtxt(dpath_tf_data, dtype=str)
+
+            self._spath_result_matrix = spath_result_matrix
 
         self._mate = mate.MATE()
 
@@ -83,31 +96,54 @@ class FastTENET(object):
             device_ids=None,
             procs_per_device=None,
             batch_size=None,
-            num_kernels=3,
-            method='interpolation',
+            num_kernels=1,
+            method='pushing',
             kp=0.5,
             percentile=0,
             win_length=10,
             polyorder=3,
             kw_smooth=True,
             data_smooth=True,
-            dt=1
+            dt=1,
+            config=None
             ):
 
         if not device:
-            device = "cpu"
+            if config:
+                device = config['DEVICE']
+            else:
+                device = "cpu"
 
         if not device_ids:
-            if 'cpu' in device:
-                device_ids = [0]
+            if config:
+                device_ids = int(config['DEVICE_IDS'])
             else:
-                device_ids = get_device_list()
+                if 'cpu' in device:
+                    device_ids = [0]
+                else:
+                    device_ids = get_device_list()
 
         if not procs_per_device:
-            procs_per_device = 1
+            if config:
+                procs_per_device = int(config['PROCS_PER_DEVICE'])
+            else:
+                procs_per_device = 1
 
         if not batch_size:
-            raise ValueError("batch size should be refined")
+            if config:
+                batch_string = config['BATCH_SIZE'].split('**')
+                if len(batch_string) > 1:
+                    batch_size = int(batch_string[0]) ** int(batch_string[1])
+                else:
+                    batch_size = int(batch_string[0])
+            else:
+                raise ValueError("batch size should be refined")
+
+        if config:
+            method = config['METHOD']
+            num_kernels = int(config['NUM_KERNELS'])
+            kp = float(config['KP'])
+            dt = int(config['DT'])
 
         arr = self.refine_data()
 
