4cd289ed05542622fcaf54d83ec597cd67396eee
rbsung <rakbin007@naver.com>
2024-11-24 15:06:40 +0900

    update version

--- fasttenet/fasttenet.py
+++ fasttenet/fasttenet.py
@@ -147,10 +147,11 @@ class FastTENET(object):
         if config:
             binning_method = config['BINNINGMETHOD']
             kp = float(config['KP'])
-            dt = int(config['DT'])
 
             if 'NUM_KERNELS' in config:
                 num_kernels = int(config['NUM_KERNELS'])
+            if 'DT' in config:
+                dt = int(config['DT'])
             if 'BINNINGOPT' in config:
                 binning_opt = config['BINNINGOPT']
             if 'SMOOTHOPT' in config:
