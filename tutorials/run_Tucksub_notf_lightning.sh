034c9dd5a02205c5dc3b18c91afff0c672f1623e
rbsung <rakbin007@naver.com>
2026-01-02 14:21:00 +0900

    delete deprecated tutorial files

--- tutorials/run_Tucksub_notf_lightning.sh
+++
@@ -1,7 +0,0 @@
-python tutorial_notf.py\
- --fp_exp expression_dataTuck_sub.csv\
- --fp_trj pseudotimeTuck.txt\
- --fp_br cell_selectTuck.txt\
- --num_devices 8\
- --batch_size 32768\
- --sp_rm TE_result_matrix.txt
--- tutorials/run_skin_tf_lightning.sh
+++
@@ -1,8 +0,0 @@
- python tutorial_tf.py\
- --fp_exp skin_exp_data.csv\
- --fp_trj skin_pseudotime.txt\
- --fp_br skin_cellselect.txt\
- --fp_tf human_tfs.txt\
- --num_devices 8\
- --batch_size 2048\
- --sp_rm TE_result_matrix.txt
